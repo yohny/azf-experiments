9aaaff1f3e593b0b9788958fc4b1feafb850196a
Ján Neščivera <jan.nescivera@gmail.com>
2024-09-30 15:58:24 +0200

    use common header name for userId

--- src/functions/negotiate.ts
+++ src/functions/negotiate.ts
@@ -11,23 +11,24 @@ const inputSignalR = input.generic({
   type: "signalRConnectionInfo",
   name: "connectionInfo",
   hubName: "serverless",
-  userId: "{headers.x-ms-signalr-userid}",
-  //connectionStringSetting: 'SIGNALR_CONNECTION_STRING', // only if we want to override connection string name, default is AzureSignalRConnectionString
+  userId: "{headers.x-ms-client-principal-name}", // normally filled by Azure when using Azure auth., but in this sample we are adding it on clinets manually
+  //idToken: "{headers.authorization}", // needs to remove 'Bearer ' from header to parse JWT successfully
+  //claimTypeList: ["name", "email"], // claims are sucessfully propagated to acess token, but SignalR does not recognize/use them as user identity
+  //connectionStringSetting: 'SIGNALR_CONNECTION_STRING', // only if we want to override connection string setting name, default is AzureSignalRConnectionString
 });
 
 async function negotiate(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  //request.user is null, maybe with proper auth it would be filled
-  const user = request.headers.get("x-ms-signalr-userid");
+  //request.user is null, maybe with proper auth it would be filled?
+  const user = request.headers.get("x-ms-client-principal-name");
   if (!user) {
     return { status: 400, body: "Missing user" };
   }
 
   const tss = new TableStorageService();
   if (!(await tss.pendingOrActiveSupportRequestExists(user))) {
-    // TODO: get user from request
     return { status: 403, body: "Unauthorized" };
   }
 
