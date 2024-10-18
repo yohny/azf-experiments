c169d3b03d798ed7cee9adf18bc18baa49210cd7
Ján Neščivera <jan.nescivera@gmail.com>
2024-10-18 17:28:44 +0200

    connection restore with timeout using durable function

--- src/functions/durableHello1.ts
+++ src/functions/durableHello1.ts
@@ -2,53 +2,91 @@ import {
   app,
   HttpHandler,
   HttpRequest,
-  HttpResponse,
+  HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
-import * as df from "durable-functions";
-import {
-  ActivityHandler,
-  OrchestrationContext,
-  OrchestrationHandler,
-} from "durable-functions";
+import { getClient, input, app as dfApp } from "durable-functions";
+import { OrchestrationContext, OrchestrationHandler } from "durable-functions";
 
-const activityName = "durableHello1";
-
-const durableHello1Orchestrator: OrchestrationHandler = function* (
+const connectionDroppedOrchestrator: OrchestrationHandler = function* (
   context: OrchestrationContext
 ) {
-  const outputs = [];
-  outputs.push(yield context.df.callActivity(activityName, "Tokyo"));
-  outputs.push(yield context.df.callActivity(activityName, "Seattle"));
-  outputs.push(yield context.df.callActivity(activityName, "Cairo"));
-
-  return outputs;
-};
-df.app.orchestration("durableHello1Orchestrator", durableHello1Orchestrator);
-
-const durableHello1: ActivityHandler = (input: string): string => {
-  return `Hello, ${input}`;
-};
-df.app.activity(activityName, { handler: durableHello1 });
-
-const durableHello1HttpStart: HttpHandler = async (
-  request: HttpRequest,
-  context: InvocationContext
-): Promise<HttpResponse> => {
-  const client = df.getClient(context);
-  const body: unknown = await request.text();
-  const instanceId: string = await client.startNew(
-    request.params.orchestratorName,
-    { input: body }
+  // The connection has 2 min to recover
+  const expiration = new Date(
+    context.df.currentUtcDateTime.valueOf() + 2 * 60 * 1000
   );
 
-  context.log(`Started orchestration with ID = '${instanceId}'.`);
+  const timeoutTask = context.df.createTimer(expiration);
+  const connectionrestoredTask =
+    context.df.waitForExternalEvent("ConnectionRestored");
 
-  return client.createCheckStatusResponse(request, instanceId);
+  const winner = yield context.df.Task.any([
+    connectionrestoredTask,
+    timeoutTask,
+  ]);
+
+  if (winner === timeoutTask) {
+    // Timeout expired without reconnection - return disconected (true)
+    return true;
+  }
+
+  // Connection restored - return connected (false)
+  if (!timeoutTask.isCompleted) {
+    // All pending timers must be complete or canceled before the function exits.
+    timeoutTask.cancel();
+  }
+  return false;
 };
 
-app.http("durableHello1HttpStart", {
-  route: "orchestrators/{orchestratorName}",
-  extraInputs: [df.input.durableClient()],
-  handler: durableHello1HttpStart,
+dfApp.orchestration(
+  "connectionDroppedOrchestrator",
+  connectionDroppedOrchestrator
+);
+
+const connectionStateChangeHandler: HttpHandler = async (
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> => {
+  if (!request.params.state || !request.params.connectionId) {
+    return {
+      status: 400,
+      body: "Misisng state or connectionId",
+    };
+  }
+  const state = request.params.state;
+  if (state !== "on" && state !== "off") {
+    return {
+      status: 400,
+      body: "Invalid connection state value",
+    };
+  }
+
+  const client = getClient(context);
+
+  if (state === "off") {
+    const instanceId = await client.startNew("connectionDroppedOrchestrator", {
+      instanceId: request.params.connectionId,
+    });
+    context.log(
+      `Started 'connectionDroppedOrchestrator' with ID = '${instanceId}'.`
+    );
+    return client.createCheckStatusResponse(request, instanceId);
+  } else {
+    try {
+      await client.raiseEvent(
+        request.params.connectionId,
+        "ConnectionRestored",
+        null
+      );
+    } catch (error) {
+      context.error(error);
+    }
+    return { status: 202, body: "Connection restored event raised" };
+  }
+};
+
+app.http("durableConnectionState", {
+  route: "connection/{connectionId}/{state}",
+  extraInputs: [input.durableClient()],
+  handler: connectionStateChangeHandler,
 });
